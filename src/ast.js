729fff6cbb99869cdda80fafc33db178d4002a1a
H. H. Hassan <Ulq1517@users.noreply.github.com>
2021-04-03 14:33:47 +0300

    Update ast.js

--- src/ast.js
+++ src/ast.js
@@ -4,7 +4,7 @@ import util from "util"
 
 export class Program {
   constructor(id, classBody) {
-    Object.assign(this, {id, classBody})
+    Object.assign(this, { id, classBody })
   }
   [util.inspect.custom]() {
     return formatAST(this)
@@ -13,133 +13,133 @@ export class Program {
 
 export class FunDec {
   constructor(id, parameters, returnType, body) {
-    Object.assign(this, {id, parameters, returnType, body})
+    Object.assign(this, { id, parameters, returnType, body })
   }
 }
 
 export class Param {
   constructor(type, id) {
-    Object.assign(this, {type, id})
+    Object.assign(this, { type, id })
   }
 }
 
 export class FunCall {
   constructor(id, parameters) {
-    Object.assign(this, {id, parameters})
+    Object.assign(this, { id, parameters })
   }
 }
 
 export class VarInitializer {
   constructor(type, assignment) {
-    Object.assign(this, {type, assignment})
+    Object.assign(this, { type, assignment })
   }
 }
 
 export class VarDec {
   constructor(type, id) {
-    Object.assign(this, {type, id})
+    Object.assign(this, { type, id })
   }
 }
 
 export class ReturnStatement {
   constructor(expression) {
-    Object.assign(this, {expression})
+    Object.assign(this, { expression })
   }
 }
 
 export class PrintStatement {
   constructor(argument) {
-    Object.assign(this, {argument})
+    Object.assign(this, { argument })
   }
 }
 
 export class BinaryExpression {
   constructor(op, left, right) {
-    Object.assign(this, {left, op, right})
+    Object.assign(this, { left, op, right })
   }
 }
 
 export class PrefixExpression {
   constructor(op, operand) {
-    Object.assign(this, {op, operand})
+    Object.assign(this, { op, operand })
   }
 }
 
 export class PostfixExpression {
   constructor(op, operand) {
-    Object.assign(this, {operand, op})
+    Object.assign(this, { operand, op })
   }
 }
 
 export class ArrayLiteral {
   constructor(list) {
-    Object.assign(this, {list})
+    Object.assign(this, { list })
   }
 }
 
 export class Assignment {
   constructor(target, source) {
-    Object.assign(this, {target, source})
+    Object.assign(this, { target, source })
   }
 }
 
 export class ArrayVar {
   constructor(id, indexExp) {
-    Object.assign(this, {id, indexExp})
+    Object.assign(this, { id, indexExp })
   }
 }
 
 export class DictionaryAccess {
   constructor(id, key) {
-    Object.assign(this, {id, key})
+    Object.assign(this, { id, key })
   }
 }
 
 export class Conditional {
   constructor(ifStatement, elseIfStatements, elseStatement) {
-    Object.assign(this, {ifStatement, elseIfStatements, elseStatement})
+    Object.assign(this, { ifStatement, elseIfStatements, elseStatement })
   }
 }
 
 export class ConditionalIF {
   constructor(exp, body) {
-    Object.assign(this, {exp, body})
+    Object.assign(this, { exp, body })
   }
 }
 
 export class ConditionalELSEIF {
   constructor(exp, body) {
-    Object.assign(this, {exp, body})
+    Object.assign(this, { exp, body })
   }
 }
 
 export class ConditionalELSE {
   constructor(body) {
-    Object.assign(this, {body})
+    Object.assign(this, { body })
   }
 }
 
 export class Loop {
   constructor(condition, body) {
-    Object.assign(this, {condition, body})
+    Object.assign(this, { condition, body })
   }
 }
 
 export class DoLoop {
   constructor(iterator, range, steps, body) {
-    Object.assign(this, {iterator, range, steps, body})
+    Object.assign(this, { iterator, range, steps, body })
   }
 }
 
 export class IdExp {
   constructor(name) {
-    Object.assign(this, {name})
+    Object.assign(this, { name })
   }
 }
 
 export class Variable {
   constructor(id, type) {
-    Object.assign(this, {id, type})
+    Object.assign(this, { id, type })
   }
 }
 
